c22a9ecd1a502abf93176dfd63d1fdf49908be84
Ava Scherocman <ava@scherocman.com>
2019-09-25 10:32:57 -0400

    Replace JLabel salary table with JTable

--- TeacherPayroll/src/TeacherPayroll.java
+++ TeacherPayroll/src/TeacherPayroll.java
@@ -8,20 +8,20 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class TeacherPayroll {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         //create GUI
         JFrame GUI = new JFrame();
         //create string of titles to apply to labels
-        String[]labelTitles = {"Base Salary", "Education Level", "Years Teaching"};
+        String[] labelTitles = {"Base Salary", "Education Level", "Years Teaching"};
         //create array of labels
         JLabel[] labels = new JLabel[3];
         //create array of text fields for input
         JTextField[] textFields = new JTextField[7];
         //loop through all categories and add arrays of text fields and labels to GUI
-        for (int i = 0; i<labels.length; i++){
-            labels[i]= new JLabel(labelTitles[i]);
+        for (int i = 0; i < labels.length; i++) {
+            labels[i] = new JLabel(labelTitles[i]);
             GUI.add(labels[i]);
-            textFields[i]=new JTextField("", 25);
+            textFields[i] = new JTextField("", 25);
             GUI.add(textFields[i]);
         }
         //create menu buttons
@@ -34,13 +34,13 @@ public class TeacherPayroll {
         GUI.setLayout(new FlowLayout());
         //set GUI settings
         GUI.setResizable(true);
-        GUI.setSize(300,250);
+        GUI.setSize(300, 250);
         GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         GUI.setVisible(true);
 
         calculate.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-               calculatePay(Integer.parseInt(textFields[0].getText()), Integer.parseInt(textFields[1].getText()), Integer.parseInt(textFields[2].getText()));
+                calculatePay(Integer.parseInt(textFields[0].getText()), Integer.parseInt(textFields[1].getText()), Integer.parseInt(textFields[2].getText()));
                 showTable(Integer.parseInt(textFields[0].getText()));
             }
         });
@@ -49,18 +49,18 @@ public class TeacherPayroll {
             public void actionPerformed(ActionEvent e) {
                 int base = 0;
                 int level = 0;
-                int years =0;
+                int years = 0;
                 String strLine = "";
                 try {
                     int i = 0;
                     BufferedReader br = new BufferedReader(new FileReader("src/teacherpayroll.txt"));
-                    while (i<3) {
+                    while (i < 3) {
                         strLine = br.readLine();
-                        if (i==0){
+                        if (i == 0) {
                             base = Integer.parseInt(strLine);
-                        } else if (i==1){
+                        } else if (i == 1) {
                             level = Integer.parseInt(strLine);
-                        } else if (i==2){
+                        } else if (i == 2) {
                             years = Integer.parseInt(strLine);
                         }
                         i++;
@@ -79,19 +79,28 @@ public class TeacherPayroll {
             }
         });
     }
-    public static int calculatePay(int base, int level, int years){
+
+    public static int calculatePay(int base, int level, int years) {
         //create GUI
         JFrame GUI = new JFrame();
-        int pay= base + (500*level)+(1000*years);
-        JOptionPane.showMessageDialog(GUI, ("$")+ Integer.toString(pay), "Your pay", JOptionPane.INFORMATION_MESSAGE);
+        int pay = base + (500 * level) + (1000 * years);
+        JOptionPane.showMessageDialog(GUI, ("$") + Integer.toString(pay), "Your pay", JOptionPane.INFORMATION_MESSAGE);
         return pay;
     }
 
-    public static void showTable(int base){
-        JFrame GUI = new JFrame();
-        //create array of labels for displaying column titles
+    public static void showTable(int base) {
+
+                Frame GUI = new JFrame();
+        String[] tableTitles = new String[]{"Years", "Bachelors", "Masters", "PhD"};
+        JTable table = new JTable(getSalaryTable(base),tableTitles);
+        // JTable properties.
+        table.setLocation(0,0);
+        table.setSize(400,335);
+        // Add table to pane2.
+        GUI.add(table);
+        /*//create array of labels for displaying column titles
         JLabel[] labels = new JLabel[4];
-        GUI.setLayout(new GridLayout(20, 2));
+        GUI.setLayout(new GridLayout(20, 4));
         String[] labelTitles = {"     ", "Bachelor", "Master", "Doctorate"};
         //set GUI layout
         for (int i = 0; i < labels.length; i++) {
@@ -99,26 +108,33 @@ public class TeacherPayroll {
             GUI.add(labels[i]);
         }
 
-        for (int j=0;j<20;j++){
-            GUI.setLayout(new GridLayout(20, 2));
+        for (int j = 0; j < 20; j++) {
             JLabel num = new JLabel(Integer.toString(j));
-            JLabel b = new JLabel(Integer.toString(base + (j*1000)));
-            JLabel m = new JLabel(Integer.toString(base+500+(j*1000)));
-            JLabel d = new JLabel(Integer.toString(base+1000+(j*1000)));
+            JLabel b = new JLabel();
+            JLabel m = new JLabel(Integer.toString(base + 500 + (j * 1000)));
+            JLabel d = new JLabel(Integer.toString(base + 1000 + (j * 1000)));
             GUI.add(num);
             GUI.add(b);
             GUI.add(m);
             GUI.add(d);
 
-     }
+        }*/
         GUI.setResizable(true);
-        GUI.setSize(500,500);
-        GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI.setSize(500, 500);
         GUI.setVisible(true);
 
 
+    }
 
-
-
+   public static String[][] getSalaryTable(int base) {
+        // Create table of salaries.
+        String[][] table = new String[21][4];
+        for (int i = 0; i <= 20; i++) {
+            table[i][0] = String.valueOf(i);
+            table[i][1] = String.valueOf(Integer.toString(base));
+            table[i][2] = String.valueOf((Integer.toString(base + 1000 + (i * 1000)))); // Master's degree.
+            table[i][3] = String.valueOf(Integer.toString(base + 1000 + (i * 1000)));
+        }
+        return table;
     }
 }
