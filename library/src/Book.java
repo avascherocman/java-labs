09912bdfc60629bfebc3f8a2c97d0d70d5e792fc
Ava Scherocman <ava@scherocman.com>
2019-10-30 00:07:47 -0400

    Add classes & most functionality to library lab

--- library/src/Book.java
+++ library/src/Book.java
@@ -9,5 +9,15 @@ public class Book {
         this.author=a;
     }
 
+    public static boolean isBorrowed(Book b){
+        return b.borrowed;
+    }
 
+    public static String getTitle(Book b){
+        return b.title;
+    }
+
+    public static String getAuthor(Book b){
+        return b.author;
+    }
 }
